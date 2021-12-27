01a63dd3a661ddee076226a19bd43490d6145571
Tarasa24 <tarasa24@tarasa24.dev>
2021-12-27 14:20:48 +0100

    backend - added model sync

--- web-backend/src/index.js
+++ web-backend/src/index.js
@@ -1,6 +1,16 @@
 const app = require('./app')
+const Repo = require('./models/Repo')
 
-const port = 8081
-app.listen(port, () =>
-  console.log(`<Website Backend server> listening on port ${port}`)
-)
+async function main() {
+  // Sync all models
+  await Repo.sync()
+  console.log("All models were synchronized successfully.");
+
+  // Run http server
+  const port = 8081
+  app.listen(port, () =>
+    console.log(`<Website Backend server> listening on port ${port}`)
+  )
+}
+
+main()

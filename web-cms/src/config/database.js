c754cf61bffef333bf24da83f96501dda2f57326
Tarasa24 <tarasa24@tarasa24.dev>
2022-01-24 00:21:40 +0100

    Update database.js

--- web-cms/src/config/database.js
+++ web-cms/src/config/database.js
@@ -12,6 +12,7 @@ module.exports = ({ env }) => {
           database: env("DATABASE_NAME", "strapi"),
           user: env("DATABASE_USERNAME", "strapi"),
           password: env("DATABASE_PASSWORD", "strapi"),
+          charset: 'utf8mb4',
           ssl: {
             rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
           },

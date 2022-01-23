2ea4aca74df88598d270e76dc3ee5d3a8dd290c7
Tarasa24 <tarasa24@tarasa24.dev>
2022-01-23 23:01:25 +0100

    Update server.js

--- web-cms/src/config/server.js
+++ web-cms/src/config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: process.env.NODE_ENV == 'production' ? '/cms' : undefined
+  url: process.env.NODE_ENV == 'production' ? 'https://tarasa24.dev/cms' : undefined
 });

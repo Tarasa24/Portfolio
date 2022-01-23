cc2fa360e8c077f3f297ac60c371ed49033b57a4
Tarasa24 <tarasa24@tarasa24.dev>
2022-01-23 19:57:06 +0100

    Backend rework

--- web-backend/src/index.js
+++ web-backend/src/index.js
@@ -1,12 +1,6 @@
 const app = require('./app')
-const Repo = require('./models/Repo')
 
 async function main() {
-  // Sync all models
-  await Repo.sync()
-  console.log('All models were synchronized successfully.')
-
-  // Run http server
   const port = 8081
   app.listen(port, () =>
     console.log(`<Website Backend server> listening on port ${port}`)
--- web-backend/src/routes.js
+++ web-backend/src/routes.js
@@ -1,8 +1,6 @@
-const gitSync = require('./routes/gitSync.js')
 const contact = require('./routes/contact.js')
 
 module.exports = (app) => {
-  app.get('/gitSync', gitSync)
   app.post('/contact', contact)
 
   return app

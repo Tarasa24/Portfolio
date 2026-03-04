0d22fb5457d6a052ce44d64d50b532e801a1bce2
Tarasa24 <tarasa24@tarasa24.dev>
2026-03-04 22:25:16 +0100

    Update packages

--- devenv.nix
+++ devenv.nix
@@ -1,10 +1,15 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  packages = [ pkgs.git pkgs.hugo ];
+  packages = [
+    pkgs.git pkgs.hugo pkgs.dart-sass
+    pkgs.wrangler
+  ];
 
   languages.go.enable = true;
-
+  languages.python.enable = true;
+  languages.python.venv.enable = true;
+  languages.python.venv.requirements = builtins.readFile ./requirements.txt;
   enterShell = ''
   '';
 }
